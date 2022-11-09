a1279e25c07ca975c466076ce022025ef6d50197
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2022-11-08 22:20:33 -0800

    First Commit

---
+++ Backend/Routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getUsers, Register, Login, Logout } from "../Controllers/Users.js";
+import { verifyToken } from "../Middleware/VerifyToken.js";
+import { refreshToken } from "../Controllers/RefreshToken.js";
+ 
+const router = express.Router();
+ 
+router.get('/users', verifyToken, getUsers);
+router.post('/users', Register);
+router.post('/login', Login);
+router.get('/token', refreshToken);
+router.delete('/logout', Logout);
+ 
+export default router;
---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import db from "./Config/Database.js";
+import router from "./Routes/index.js";
+dotenv.config();
+const app = express();
+ 
+app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(router);
+ 
+app.listen(4000, ()=> console.log('Server running at port 4000'));
