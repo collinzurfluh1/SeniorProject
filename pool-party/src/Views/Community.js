ffafac18cc01ae6dd6854ce9f2fb15019c6e216e
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2022-11-21 09:36:41 -0800

    Added material UI and finished homepage

---
+++ pool-party/src/Views/Community.js
@@ -0,0 +1,16 @@
+import NavigationBar from '../Modules/NavigationBar';
+import Footer from '../Modules/Footer';
+import CommunityBody from '../Modules/CommunityBody';
+
+
+function Community() {
+  return (
+    <div id="Home">
+        <NavigationBar />
+        <CommunityBody />
+        <Footer />
+    </div>
+  );
+}
+
+export default Community;
---
+++ pool-party/src/Views/Creator.js
@@ -0,0 +1,16 @@
+import CreatorBody from '../Modules/CreatorBody';
+import NavigationBar from '../Modules/NavigationBar';
+import Footer from '../Modules/Footer';
+
+
+function Creator() {
+  return (
+    <div id="Home">
+        <NavigationBar />
+        <CreatorBody />
+        <Footer />
+    </div>
+  );
+}
+
+export default Creator;
