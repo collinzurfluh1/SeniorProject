04e300107685dd89278aea88d034350007b96451
devinhelgeson <devinhelgeson@gmail.com>
2022-11-21 12:08:01 -0800

    Initialize Wiring Table, Changes to PipingModel

--- Backend/Models/WiringModel.js
+++ Backend/Models/WiringModel.js
@@ -3,19 +3,19 @@ import db from "../config/Database.js";
  
 const { DataTypes } = Sequelize;
  
-const Piping = db.define('piping',{
+const Wiring = db.define('wiring',{
     name:{
         type: DataTypes.STRING,
         unique: {
             args: true,
-            msg: 'pipe name already exists.'
+            msg: 'wire name already exists.'
         }
     },
     type:{
         type: DataTypes.STRING,
     },
-    size:{
-        type: DataTypes.double 
+    gauge:{
+        type: DataTypes.double
     },
     linft_cost:{
         type: DataTypes.double
@@ -28,4 +28,4 @@ const Piping = db.define('piping',{
     await db.sync();
 })();
  
-export default Piping;
+export default Wiring;
