b7c2a78ed9945d68da1ff327650ef1d2b97f78bb
devinhelgeson <devinhelgeson@gmail.com>
2023-01-09 12:46:48 -0800

    Fix Chemicals Dump Schema

--- Backend/Models/ChemicalsModel.js
+++ Backend/Models/ChemicalsModel.js
@@ -26,19 +26,19 @@ const Chemicals = db.define('chemicals',{
 })();
 
 const chemicals_data = [
-    {'name': 'In The Swim Chlorine Tablets', 'type': 'chlorine', 'pound_cost': 0.62},
-    {'name': 'RAYYAKICG 3 in tablets', 'type': 'chlorine', 'pound_cost': 0.50},
-    {'name': 'HTH Pool Care 1 in tablets', 'type': 'chlorine', 'pound_cost': 0.78},
-    {'name': 'Clorox Pool and SpaXtrablue 3 in tablets', 'type': 'chlorine', 'pound_cost': 0.30},
-    {'name': 'RAYYAKICG 1 in Tablets', 'type': 'chlorine', 'pound_cost': 0.72},
-    {'name': 'Pool Mate Stabilizer and Conditioner', 'type': 'cyanuric acid', 'pound_cost': 0.39},
-    {'name': 'Puri Tect Stabilizer Conditioner', 'type': 'cyanuric acid', 'pound_cost': 0.20},
-    {'name': 'Rx Clear Swimming Pool Stabilizer and Conditioner', 'type': 'cyanuric acid', 'pound_cost': 0.22},
-    {'name': 'Leisure Pool Chlorine Stabilizer', 'type': 'cyanuric acid', 'pound_cost': 0.40},
-    {'name': 'HTH Pool Care Shock', 'type': 'shock', 'pound_cost': 0.27},
-    {'name': 'In The Swim Super Pool Shock', 'type': 'shock', 'pound_cost': 0.49},
-    {'name': 'Leslie\'s Power Powder', 'type': 'shock', 'pound_cost': 0.42},
-    {'name': 'Clorox Pool Aand Spa Pool Shock Xtrablue ', 'type': 'shock', 'pound_cost': 0.48}
+    {'name': 'In The Swim Chlorine Tablets', 'type': 'chlorine', 'ounce_cost': 0.62},
+    {'name': 'RAYYAKICG 3 in tablets', 'type': 'chlorine', 'ounce_cost': 0.50},
+    {'name': 'HTH Pool Care 1 in tablets', 'type': 'chlorine', 'ounce_cost': 0.78},
+    {'name': 'Clorox Pool and SpaXtrablue 3 in tablets', 'type': 'chlorine', 'ounce_cost': 0.30},
+    {'name': 'RAYYAKICG 1 in Tablets', 'type': 'chlorine', 'ounce_cost': 0.72},
+    {'name': 'Pool Mate Stabilizer and Conditioner', 'type': 'cyanuric acid', 'ounce_cost': 0.39},
+    {'name': 'Puri Tect Stabilizer Conditioner', 'type': 'cyanuric acid', 'ounce_cost': 0.20},
+    {'name': 'Rx Clear Swimming Pool Stabilizer and Conditioner', 'type': 'cyanuric acid', 'ounce_cost': 0.22},
+    {'name': 'Leisure Pool Chlorine Stabilizer', 'type': 'cyanuric acid', 'ounce_cost': 0.40},
+    {'name': 'HTH Pool Care Shock', 'type': 'shock', 'ounce_cost': 0.27},
+    {'name': 'In The Swim Super Pool Shock', 'type': 'shock', 'ounce_cost': 0.49},
+    {'name': 'Leslie\'s Power Powder', 'type': 'shock', 'ounce_cost': 0.42},
+    {'name': 'Clorox Pool Aand Spa Pool Shock Xtrablue ', 'type': 'shock', 'ounce_cost': 0.48}
    ]
 
 db.sync({ force: true }).then(() => {
