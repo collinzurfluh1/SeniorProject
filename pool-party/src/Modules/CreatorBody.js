17e6303891e12029fbbbd88a14312f21a978d793
rjd_gamer <jman248@comcast.net>
2023-01-11 13:07:47 -0800

    Static Body Implemented, Next: Dynamicy

--- pool-party/src/Modules/CreatorBody.js
+++ pool-party/src/Modules/CreatorBody.js
@@ -15,19 +15,25 @@ function CreatorBody() {
         <div className="heroText">
           <h1>Creator Portal</h1>
         </div>
-        <div>
+        <div class="Pool-Elements">
         <h1>Choose Pool Shape</h1>
 
         <form>
-          <input type="radio" id="circle" name="pool_shape"></input>
-          <label for="pool_shape">Circular Pool</label>
-          <input type="radio" id="square" name="pool_shape"></input>
-          <label for="pool_shpae">Square Pool</label>
-          <input type="radio" id="rectangle" name="pool_shape"></input>
-          <label for="pool_shape">Circular Pool</label>
-          <button type="button">Prev</button>
-          <button type="button">Next</button>
+          <input type="radio" id="circle" class="Next-Button" name="pool_shape"></input>
+          <label for="pool_shape">Circular Pool  </label>
+          <input type="radio" id="square"j class="Next-Button" name="pool_shape"></input>
+          <label for="pool_shpae">Square Pool  </label>
+          <input type="radio" id="rectangle" class="Next-Button" name="pool_shape"></input>
+          <label for="pool_shape">Rectangular Pool  </label>
+          <br></br>
+          <button type="button" class="Prev-Button">Prev</button>
+          <button type="button" class="Next-Button">Next</button>
          </form>
+        </div>
+        <div>
+          <script>console.log("You have the BIG GAY!!");</script>
+
+
         </div>
 
   </div>
