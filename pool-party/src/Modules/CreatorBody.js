57a59668d5a55a5cd9058141332f9785bb526460
rjd_gamer <jman248@comcast.net>
2023-01-11 12:16:13 -0800

    Started Creator Body

--- pool-party/src/Modules/CreatorBody.js
+++ pool-party/src/Modules/CreatorBody.js
@@ -15,6 +15,21 @@ function CreatorBody() {
         <div className="heroText">
           <h1>Creator Portal</h1>
         </div>
+        <div>
+        <h1>Choose Pool Shape</h1>
+
+        <form>
+          <input type="radio" id="circle" name="pool_shape"></input>
+          <label for="pool_shape">Circular Pool</label>
+          <input type="radio" id="square" name="pool_shape"></input>
+          <label for="pool_shpae">Square Pool</label>
+          <input type="radio" id="rectangle" name="pool_shape"></input>
+          <label for="pool_shape">Circular Pool</label>
+          <button type="button">Prev</button>
+          <button type="button">Next</button>
+         </form>
+        </div>
+
   </div>
   );
 }
