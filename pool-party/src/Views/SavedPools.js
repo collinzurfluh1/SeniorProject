fce763f705927ba4b893f5eb8cd0e6ab06efda6f
collinzurfluh1 <fantasychroniclesmc@gmail.com>
2023-01-23 17:17:18 -0800

    Added saved pools

---
+++ pool-party/src/Views/SavedPools.js
@@ -0,0 +1,18 @@
+import NavigationBar from '../Modules/NavigationBar';
+import Footer from '../Modules/Footer';
+import CommunityBody from '../Modules/CommunityBody';
+import SmallHeader from '../Modules/SmallHeader';
+
+
+function SavedPools() {
+  return (
+    <div id="Community">
+        <NavigationBar />
+        <SmallHeader />
+        <CommunityBody />
+        <Footer />
+    </div>
+  );
+}
+
+export default SavedPools;
