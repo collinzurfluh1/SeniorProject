9bd0a4c78c2ad92a9d63781a5a0b5b57e89ff631
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-01 19:14:18 -0800

    Added N/A formatting to pool items

--- pool-party/src/Modules/PoolItem.js
+++ pool-party/src/Modules/PoolItem.js
@@ -40,19 +40,45 @@ function PoolItem(props) {
             <h4>{props.pool.pool.title}</h4>
         </div>
         <div className='poolStatsList'>
-            <div className='poolStat'>Price: ${props.pool.pool.cost}</div>
-            <div className='poolStat'>Length: {props.pool.pool.length}ft</div>
-            <div className='poolStat'>Width: {props.pool.pool.width}ft</div>
-            <div className='poolStat'>Depth Shallow: {props.pool.pool.depth_shallow}ft</div>
-            <div className='poolStat'>Depth Deep: {props.pool.pool.depth_deep}ft</div>
-            <div className='poolStat'>Slant Type: {props.pool.pool.slant_type}</div>
-            <div className='poolStat'>Lining Type: {props.pool.pool.lining_type}</div>
-            <div className='poolStat'>Cover 1: {props.pool.pool.cover1}</div>
-            <div className='poolStat'>Cover 2:{props.pool.pool.cover2}</div>
-            <div className='poolStat'>Piping: {props.pool.pool.piping}</div>
-            <div className='poolStat'>Drain: {props.pool.pool.drain}</div>
-            <div className='poolStat'>Skimmer: {props.pool.pool.skimmer}</div>
-            <div className='poolStat'>Pump: {props.pool.pool.pump}</div>
+            <div className='poolStat'>Price: ${props.pool.pool.cost == null ?
+          "N/A"
+        : props.pool.pool.cost}</div>
+            <div className='poolStat'>Length: {props.pool.pool.length == null ?
+          "N/A"
+        : props.pool.pool.length}ft</div>
+            <div className='poolStat'>Width: {props.pool.pool.width == null ?
+          "N/A"
+        : props.pool.pool.width}ft</div>
+            <div className='poolStat'>Depth Shallow: {props.pool.pool.depth_shallow == null ?
+          "N/A"
+        : props.pool.pool.depth_shallow}ft</div>
+            <div className='poolStat'>Depth Deep: {props.pool.pool.depth_deep == null ?
+          "N/A"
+        : props.pool.pool.depth_deep}ft</div>
+            <div className='poolStat'>Slant Type: {props.pool.pool.slant_type == null ?
+          "N/A"
+        : props.pool.pool.slant_type}</div>
+            <div className='poolStat'>Lining Type: {props.pool.pool.lining_type == null ?
+          "N/A"
+        : props.pool.pool.lining_type}</div>
+            <div className='poolStat'>Cover 1: {props.pool.pool.cover1 == null ?
+          "N/A"
+        : props.pool.pool.cover1}</div>
+            <div className='poolStat'>Cover 2: {props.pool.pool.cover2 == null ?
+          "N/A"
+        : props.pool.pool.cover2}</div>
+            <div className='poolStat'>Piping: {props.pool.pool.piping == null ?
+          "N/A"
+        : props.pool.pool.piping}</div>
+            <div className='poolStat'>Drain: {props.pool.pool.drain == null ?
+          "N/A"
+        : props.pool.pool.drain}</div>
+            <div className='poolStat'>Skimmer: {props.pool.pool.skimmer == null ?
+          "N/A"
+        : props.pool.pool.skimmer}</div>
+            <div className='poolStat'>Pump: {props.pool.pool.pump == null ?
+          "N/A"
+        : props.pool.pool.pump}</div>
 
         </div>
       </Modal.Body>
