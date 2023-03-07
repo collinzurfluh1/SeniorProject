8e5959449465ee8d8b6ce943f1b597c7c4c151d1
devinhelgeson <devinhelgeson@gmail.com>
2023-03-06 23:13:57 -0800

    Plaster Data Access - able to access all plaster options - able to access individual plaster option with specified name - tests in index.js, must delete before pushing to main.

--- Backend/Controllers/Materials.js
+++ Backend/Controllers/Materials.js
@@ -2,4 +2,25 @@ import Cement from "../Models/CementModel.js";
 import Chemicals from "../Models/ChemicalsModel.js";
 import Piping from "../Models/PipingModel.js";
 
+export function get_plaster_data(plaster_name) {
+    // This function will return a JSON of plaster data. If you pass None you will get all plaster options. 
+    // if you pass a name, you will get only the plaster option with that name.
 
+    var plaster_data = null;
+
+    if(plaster_name == null) {
+        Cement.findAll({ where: { type: 'plaster' } })
+        .then(cement => {
+            const plaincement = cement.map(cement => cement.get({ plain: true }));
+            plaster_data = plaincement;
+        });
+    } else {
+        Cement.findAll({ where: { type: 'plaster', name: plaster_name } })
+        .then(cement => {
+            const plaincement = cement.map(cement => cement.get({ plain: true }));
+            plaster_data = plaincement;
+        });
+    }
+
+    return plaster_data
+}
