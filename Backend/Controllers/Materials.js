ad530d234e64ddc4a81688eac362fff39d9d411c
devinhelgeson <devinhelgeson@gmail.com>
2023-03-08 12:51:26 -0800

    cement data access function - acess cement by name or input null in the parameters to get all cement options - index.js continas tests so should not be  merged like this - fixed some async stuff with plaster

--- Backend/Controllers/Materials.js
+++ Backend/Controllers/Materials.js
@@ -2,25 +2,42 @@ import Cement from "../Models/CementModel.js";
 import Chemicals from "../Models/ChemicalsModel.js";
 import Piping from "../Models/PipingModel.js";
 
-export function get_plaster_data(plaster_name) {
+export async function get_plaster_data(plaster_name) {
     // This function will return a JSON of plaster data. If you pass None you will get all plaster options. 
     // if you pass a name, you will get only the plaster option with that name.
 
     var plaster_data = null;
 
     if(plaster_name == null) {
-        Cement.findAll({ where: { type: 'plaster' } })
-        .then(cement => {
-            const plaincement = cement.map(cement => cement.get({ plain: true }));
-            plaster_data = plaincement;
-        });
+        const plaster_sequelize = await Cement.findAll({ where: { type: 'plaster' } })
+        plaster_data = plaster_sequelize.map(cement => cement.get({ plain: true }));
     } else {
-        Cement.findAll({ where: { type: 'plaster', name: plaster_name } })
-        .then(cement => {
-            const plaincement = cement.map(cement => cement.get({ plain: true }));
-            plaster_data = plaincement;
-        });
+        // Cement.findAll({ where: { type: 'plaster', name: plaster_name } })
+        // .then(cement => {
+        //     const plaincement = cement.map(cement => cement.get({ plain: true }));
+        //     // var plaster_data = plaincement;
+        // });
+        const plaster_sequelize = await Cement.findAll({ where: { type: 'plaster', name: plaster_name } })
+        plaster_data = plaster_sequelize.map(cement => cement.get({ plain: true }));
     }
 
     return plaster_data
+}
+
+export async function get_cement_data(cement_name) {
+    // This function will return a JSON of cement data. If you pass None you will get all cement options. 
+    // if you pass a name, you will get only the cement option with that name.
+
+    var cement_data = null;
+
+    if(cement_name == null) {
+        const cement_sequelize = await Cement.findAll({ where: { type: 'concrete' } })
+        cement_data = cement_sequelize.map(cement => cement.get({ plain: true }));
+        
+    } else {
+            const cement_sequelize = await Cement.findAll({ where: { type: 'concrete', name: cement_name } })
+            cement_data = cement_sequelize.map(cement => cement.get({ plain: true }));;
+        }
+
+        return cement_data
 }
--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import db from "./Config/Database.js";
 import router from "./Routes/index.js";
-import { get_plaster_data } from "./Controllers/Materials.js";
+import { get_plaster_data, get_cement_data } from "./Controllers/Materials.js";
+// const { get_plaster_data } = require('/Controllers/Materials.js');
+// const { get_plaster_data } = require("./Controllers/Materials.js");
+
 dotenv.config();
 const app = express();
  
@@ -14,6 +17,12 @@ app.use(express.json());
 app.use(router);
 
 // testing material data access
-get_plaster_data('Sider Pool Plaster - 55 lb');
+// console.log('############');
+// console.log(await get_plaster_data('Sider Pool Plaster - 55 lb'));
+// console.log('############');
+
+console.log('############');
+console.log(await get_cement_data('Quikrete 50 lb. Fast-Setting Mix'));
+console.log('############');
  
 app.listen(4000, ()=> console.log('Server running at port 4000'));
