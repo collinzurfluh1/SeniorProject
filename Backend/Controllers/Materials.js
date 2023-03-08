7a0f09b5cae1352a589d953af69f627b03c78184
devinhelgeson <devinhelgeson@gmail.com>
2023-03-08 13:13:26 -0800

    covers data access Winter and solar are in sepperate functions

--- Backend/Controllers/Materials.js
+++ Backend/Controllers/Materials.js
@@ -1,6 +1,7 @@
 import Cement from "../Models/CementModel.js";
 import Chemicals from "../Models/ChemicalsModel.js";
 import Piping from "../Models/PipingModel.js";
+import Covers from "../Models/CoversModel";
 
 export async function get_plaster_data(plaster_name) {
     // This function will return a JSON of plaster data. If you pass None you will get all plaster options. 
@@ -35,9 +36,41 @@ export async function get_cement_data(cement_name) {
         cement_data = cement_sequelize.map(cement => cement.get({ plain: true }));
         
     } else {
-            const cement_sequelize = await Cement.findAll({ where: { type: 'concrete', name: cement_name } })
-            cement_data = cement_sequelize.map(cement => cement.get({ plain: true }));;
-        }
+        const cement_sequelize = await Cement.findAll({ where: { type: 'concrete', name: cement_name } })
+        cement_data = cement_sequelize.map(cement => cement.get({ plain: true }));;
+    }
 
         return cement_data
+}
+
+export async function get_winter_covers(winter_cover_name) {
+
+    var cover_data = null;
+
+    if(winter_cover_name == null) {
+        const covers_sequelize = await Covers.findAll({ where: { type: 'winter' } })
+        cover_data = covers_sequelize.map(covers => covers.get({ plain: true }));
+        
+    } else {
+        const covers_sequelize = await Covers.findAll({ where: { type: 'winter', name: winter_cover_name } })
+        cover_data = covers_sequelize.map(covers => covers.get({ plain: true }));
+    }
+
+    return cover_data
+}
+
+export async function get_solar_covers(solar_cover_name) {
+
+    var cover_data = null;
+
+    if(solar_cover_name == null) {
+        const covers_sequelize = await Covers.findAll({ where: { type: 'solar' } })
+        cover_data = covers_sequelize.map(covers => covers.get({ plain: true }));
+        
+    } else {
+        const covers_sequelize = await Covers.findAll({ where: { type: 'solar', name: solar_cover_name } })
+        cover_data = covers_sequelize.map(covers => covers.get({ plain: true }));
+    }
+
+    return cover_data
 }
