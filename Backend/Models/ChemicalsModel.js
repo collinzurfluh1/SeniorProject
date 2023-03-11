abaaf6178a6ceaaa2a8528b8ea47c99009794073
devinhelgeson <devinhelgeson@gmail.com>
2023-03-10 16:42:19 -0800

    pipe it up! added units to pipe data

--- Backend/Models/ChemicalsModel.js
+++ Backend/Models/ChemicalsModel.js
@@ -26,19 +26,19 @@ const Chemicals = db.define('chemicals',{
 // })();
 
 const chemicals_data = [
-    {'name': 'In The Swim Chlorine Tablets', 'type': 'chlorine', 'cost': 109.99},
-    {'name': 'RAYYAKICG 3 in tablets', 'type': 'chlorine', 'cost': 59.99},
-    {'name': 'HTH Pool Care 1 in tablets', 'type': 'chlorine', 'cost': 19.25},
-    {'name': 'Clorox Pool and SpaXtrablue 3 in tablets', 'type': 'chlorine', 'cost': 69.99},
-    {'name': 'RAYYAKICG 1 in Tablets', 'type': 'chlorine', 'cost': 19.99},
-    {'name': 'Pool Mate Stabilizer and Conditioner', 'type': 'cyanuric acid', 'cost': 79.99},
-    {'name': 'Puri Tect Stabilizer Conditioner', 'type': 'cyanuric acid', 'cost': 78.15},
-    {'name': 'Rx Clear Swimming Pool Stabilizer and Conditioner', 'type': 'cyanuric acid', 'cost': 74.99},
-    {'name': 'Leisure Pool Chlorine Stabilizer', 'type': 'cyanuric acid', 'cost': 64.99},
-    {'name': 'HTH Pool Care Shock', 'type': 'shock', 'cost': 51.58},
-    {'name': 'In The Swim Super Pool Shock', 'type': 'shock', 'cost': 62.99},
-    {'name': 'Leslie\'s Power Powder', 'type': 'shock', 'cost': 79.99},
-    {'name': 'Clorox Pool Aand Spa Pool Shock Xtrablue ', 'type': 'shock', 'cost': 94.50}
+    {'name': 'In The Swim Chlorine Tablets', 'type': 'chlorine', 'quantity': 20, 'cost': 109.99},
+    {'name': 'RAYYAKICG 3 in tablets', 'type': 'chlorine', 'quantity': 18, 'cost': 59.99},
+    {'name': 'HTH Pool Care 1 in tablets', 'type': 'chlorine', 'quantity': 5, 'cost': 19.25},
+    {'name': 'Clorox Pool and SpaXtrablue 3 in tablets', 'type': 'chlorine', 'quantity': 10, 'cost': 69.99},
+    {'name': 'RAYYAKICG 1 in Tablets', 'type': 'chlorine', 'quantity': 22, 'cost': 19.99},
+    {'name': 'Pool Mate Stabilizer and Conditioner', 'type': 'cyanuric acid', 'quantity': 7, 'cost': 79.99},
+    {'name': 'Puri Tect Stabilizer Conditioner', 'type': 'cyanuric acid', 'quantity': 25, 'cost': 78.15},
+    {'name': 'Rx Clear Swimming Pool Stabilizer and Conditioner', 'type': 'cyanuric acid', 'quantity': 25, 'cost': 74.99},
+    {'name': 'Leisure Pool Chlorine Stabilizer', 'type': 'cyanuric acid', 'quantity': 10, 'cost': 64.99},
+    {'name': 'HTH Pool Care Shock', 'type': 'shock', 'quantity': 12, 'cost': 51.58},
+    {'name': 'In The Swim Super Pool Shock', 'type': 'shock', 'quantity': 24, 'cost': 62.99},
+    {'name': 'Leslie\'s Power Powder', 'type': 'shock', 'quantity': 12, 'cost': 79.99},
+    {'name': 'Clorox Pool Aand Spa Pool Shock Xtrablue ', 'type': 'shock', 'quantity': 12, 'cost': 94.50}
    ]
 
 // db.sync({ force:true }).then(() => {
