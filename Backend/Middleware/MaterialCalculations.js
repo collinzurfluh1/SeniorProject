b44fe4508550d89e98f79b1ee12b53745770b69e
devinhelgeson <devinhelgeson@gmail.com>
2023-03-18 12:38:00 -0700

    if product_name is none return 0 for all price funtions

--- Backend/Middleware/MaterialCalculations.js
+++ Backend/Middleware/MaterialCalculations.js
@@ -243,6 +243,9 @@ export function calculateShockLbs(gallons)
 }
 export async function calculateShockPrice(length, width, depth, deepDepth, floorType, product_name)
 {
+
+    if (product_name == null) {return 0}
+
     var gallons = calculateGallons(length, width, depth, deepDepth, floorType)
    
     var shocklbs = calculateShockLbs(gallons);
@@ -290,6 +293,7 @@ export function calculatePipesAmount(deepDepth, length, width)
 }
 export async function calculatePipesCost(deepDepth, length, width, pipe_type)
 {
+    if (pipe_type == null) {return 0}
 
     var price_per_linear_foot =  (await get_piping(pipe_type))[0]['linft_cost'];
     // return price_per_linear_foot
@@ -355,6 +359,8 @@ export function calculateConcretePounds(length, width, depth, deepDepth, floorTy
 
 export async function calculateConcreteCost(length, width, depth, deepDepth, floorType, basinType, product_name)
 {
+
+    if (product_name == null) {return 0}
 
     var concretelbs = calculateConcretePounds(length, width, depth, deepDepth, floorType, basinType);
 
@@ -458,6 +464,8 @@ function calculateSteelWallingSqFT(length, width)
 export async function calculateSteelWallingPrice(length, width, product_name)
 {
 
+    if (product_name == null) {return 0}
+
     var sqFt = calculateSteelWallingSqFT(length, width);
     var sideWallingJson = await get_steel_walling(product_name);
 
@@ -501,6 +509,8 @@ export function calculatePoolLinerArea(length, width, depth, deepDepth, floorTyp
 export async function calculatePoolLinerPrice(length, width, depth, deepDepth, floorType, product_name)
 {
 
+    if (product_name == null) {return 0}
+
     var poolLinerSize = calculatePoolLinerArea(length, width, depth, deepDepth, floorType);
     var poolLinerJson = (await get_liner(product_name))[0];
 
@@ -541,6 +551,8 @@ export async function getAllPoolLinerPrices(length, width, depth, deepDepth, flo
 export async function calculatePoolFiberglassShellPrice(product_name)
 {
 
+    if (product_name == null) {return 0}
+
     var fiberglassShellJson = (await get_fiberglass_shell(product_name))[0];
     
     return fiberglassShellJson['cost'];
@@ -567,6 +579,8 @@ function calculatePoolWinterCoverArea(length, width)
 }
 export async function calculatePoolWinterCoverPrice(length, width, product_name)
 {
+
+    if (product_name == null) {return 0}
 
     var winterCoverSize = calculatePoolWinterCoverArea(length, width);
     var winterCoverJson = (await get_winter_covers(product_name))[0];
@@ -610,6 +624,8 @@ function calculatePoolSolarCover(length, width)
 }
 export async function calculatePoolSolarCoverPrice(length, width, product_name)
 {
+
+    if (product_name == null) {return 0}
 
     var solarCoverSize = calculatePoolSolarCover(length, width);
     var solarCoverJson = (await get_solar_covers(product_name))[0];
@@ -736,6 +752,8 @@ export function calculatePoolPumpPrice(length, width, depth, deepDepth, floorTyp
 }
 export function getPump(name)
 {
+
+    if (name == null) {return 0}
     const pumps = [
         {
           "Pumps": "Self-Priming Dual Speed In-Ground Pool Pump 2 in",
@@ -796,6 +814,9 @@ export function calculateRebar(length, width, depth)
 }
 export function getSkimmerPrice(name)
 {
+
+    if (name == null) {return 0}
+
     const skims = [
         {
           "Name": "Hayward SP1091LX Dyna-Skim Above-Ground Pool Skimmer",
@@ -847,6 +868,9 @@ export function getAllSkimmerPrices()
 }
 export function getDrainPrice(name)
 {
+
+    if (name == null) {return 0}
+
     const drains = [
         {
           "Name": "Tongoss 8 Bottom Pool Drain Cover",
@@ -912,6 +936,8 @@ export function calculatePlaster(length, width, depth, deepDepth, floorType)
 }
 export async function calculatePlasterCost(length, width, depth, deepDepth, floorType, product_name)
 {
+
+    if (product_name == null) {return 0}
 
     var plasterlbsNeeded = calculatePlaster(length, width, depth, deepDepth, floorType);
     var plasterJson = (await get_plaster_data(product_name))[0];
